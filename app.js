95c9e0d47c111943255782fd51153747c52489d6
שלמה וקסמן <shw4114123@gmail.com>
2026-07-28 17:30:34 +0300

    created the endpoints post and get of the sessions

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import usersRouter from "./routers/usersRouter.js";
+import sessionRouter from "./routers/sessionRouter.js"
 
 
 const PORT = process.env.PORT
@@ -8,6 +9,7 @@ const app = express()
 
 app.use(express.json())
 app.use("/users", usersRouter)
+app.use("/session", sessionRouter)
 
 
 
